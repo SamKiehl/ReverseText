04990200b7bb71dd73b9c91221e5d7b90c9d67b8
Sam Kiehl <e22-kiehlsg@elderhs.net>
2021-04-23 18:28:13 -0400

    Increased allowed number of results, tweaked CSS, added log statements to document what is happening.

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 let count = 0;
 function reverse(){
+    const MAX_RETURNS = 20;
     let s = document.querySelector("input").value;
     let output = "";
     for(let i = s.length; i > 0; i--){
-        if (count >= 6)
+        if (count >= MAX_RETURNS)
             break;
         output += s.substring(i - 1, i);
     }
     if(s != "")
         count++;
-    if(count === 6){
-        returned.innerHTML += `<p> Full! Please hit "Clear". </p>`;
+    if(count === MAX_RETURNS){
+        returned.innerHTML += `<p><b> Full! Please hit "Clear". </b></p>`;
         alert('Full! Please hit "Clear".');
     }
-    if(count < 6 && s != ""){
+    if(count < MAX_RETURNS && s != ""){
         console.log(output);
-        returned.innerHTML += `<p id='para${count}'> ${output} </p>`;
+        returned.innerHTML += `<p id='para${count}'>${count}.) ${output} </p>`;
+        console.log("Success");
     }
 }
 function clearR(){
     returned.innerHTML = "";
     count = 0;
+    console.log("Results have been reset.")
 }
