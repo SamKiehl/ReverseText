b6d526c66d14a26bf69ced10d0a2c897b0ef75db
Sam Kiehl <e22-kiehlsg@elderhs.net>
2021-04-27 18:34:13 -0400

    Added shuffle letters function.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let count = 0;
+const MAX_RETURNS = 20;
 
 function reverse(){
-    const MAX_RETURNS = 20;
     let s = document.querySelector("input").value;
     let output = "";
     for(let i = s.length; i > 0; i--){
@@ -25,14 +25,59 @@ function reverse(){
                 <button class = "remove" onclick = "setup();">X</button>
             </p>
         `
-        console.log("Success");
+        console.log("Success (reverse)");
     }
 }
+
 function clearR(){
     returned.innerHTML = "";
     count = 0;
     console.log("Results have been cleared.")
 }
+
+function shuffleLetters(){
+    let s = document.querySelector("input").value;
+    let output = "";
+    if(s != "")
+        count++;
+
+    if(count >= MAX_RETURNS){
+        returned.innerHTML += `<p><b> Full! Please hit "Clear". </b></p>`;
+        alert('Full! Please hit "Clear".');
+    }
+     output = shuffle(s);
+
+    if(count <= MAX_RETURNS && s != ""){
+        console.log(output);
+        returned.innerHTML += 
+        `
+            <p>
+                >\t${output}\t
+                <button class = "remove" onclick = "setup();">X</button>
+            </p>
+        `
+        console.log("Success (shuffle)");
+    }
+}
+
+function getRandomInt(n) {
+    return Math.floor(Math.random() * n);
+  }
+  function shuffle(s) {
+    var arr = s.split('');           // Convert String to array
+    var n = arr.length;              // Length of the array
+    
+    for(var i=0 ; i<n-1 ; ++i) {
+      var j = getRandomInt(n);       // Get random of [0, n-1]
+      
+      var temp = arr[i];             // Swap arr[i] and arr[j]
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    
+    s = arr.join('');                // Convert Array to string
+    return s;                        // Return shuffled string
+  }
 
 function setup() {
     var els = document.getElementsByClassName("remove");
